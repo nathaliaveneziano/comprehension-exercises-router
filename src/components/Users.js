19232f9b828b3ee54b70cd13960aea0d8e6ad65a
Nathália <nathaliaveneziano@icloud.com>
2021-03-02 20:45:13 -0300

    Exercicio 09

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,10 +1,11 @@
 // arquivo Users.js
 import React from 'react';
 
-const Users = ({ greetingMessage = 'Hi There' }) => (
+const Users = ({ greetingMessage = 'Hi There', match }) => (
   <div>
     <h2> Users </h2>
     <p> {greetingMessage}, this is my awesome Users component </p>
+    <p> Param: { match.params.id } </p>
   </div>
 );
 
