297ad5a2c9c32266c2746f5578d29a5ad2046f58
Nathália <nathaliaveneziano@icloud.com>
2021-03-02 19:32:58 -0300

    Exercicio 02

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Link } from 'react-router-dom';
 import Home from './components/Home';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/users">Users</Link>
         <Home />
       </BrowserRouter>
     );
