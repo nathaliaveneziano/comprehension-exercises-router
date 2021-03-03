d0d7098dedabfbbef4095446ac9aa80a3693f5cf
Nathália <nathaliaveneziano@icloud.com>
2021-03-02 21:52:06 -0300

    Exercicio 11

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Users from './components/Users';
+import StrictAccess from './components/StrictAccess';
 
 class App extends Component {
   render() {
@@ -11,10 +12,12 @@ class App extends Component {
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
         <Link to="/users">Users</Link>
+        <Link to="/strict-access">Strict Access</Link>
         <Switch>
           <Route exact path="/" component={ Home } />
           <Route path="/about" component={ About } />
           <Route path="/users/:id" render={ (props) => <Users {...props} greetingMessage="Good Morning" /> } />
+          <Route path="/strict-access" render={ () => <StrictAccess user={{ username: 'Joao', password: "1234"}} /> } />
         </Switch>
       </BrowserRouter>
     );
