14f5974d39f9c60de62fb3b0b4e9d490c25d3eda
Nathália <nathaliaveneziano@icloud.com>
2021-03-02 21:53:01 -0300

    Exercicio 13

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ class App extends Component {
           <Route exact path="/" component={ Home } />
           <Route path="/about" component={ About } />
           <Route path="/users/:id" render={ (props) => <Users {...props} greetingMessage="Good Morning" /> } />
+          <Route path="/strict-access" render={ () => <StrictAccess user={{ username: 'Joao', password: "1234"}} /> } />
         </Switch>
       </BrowserRouter>
     );
